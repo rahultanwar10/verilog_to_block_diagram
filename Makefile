0c29f574bc42aa49ce5ccb8e6c8e4adb6ce83602
rahultanwar10 <rahultanwarpec@gmail.com>
2024-12-01 19:05:34 +0530

    complete block diagram

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ DESIGN_DIR= design
 SCRIPT_DIR= script
 OUTPUT_DIR= output
 
+SCRIPTS := $(wildcard $(SCRIPT_DIR)/*.py)
 VERILOG_FILES := $(wildcard $(DESIGN_DIR)/*.v)
 FLATTENED_FILES := $(patsubst $(DESIGN_DIR)/%.v, $(OUTPUT_DIR)/%_flattened_verilog.v, $(VERILOG_FILES))
 SCHEMATIC_FILES := $(patsubst $(OUTPUT_DIR)/%_flattened_verilog.v, $(OUTPUT_DIR)/%_schematic.svg, $(FLATTENED_FILES))
@@ -15,17 +16,17 @@ ast: $(AST_LOG_FILES)
 
 flattened_verilog: $(FLATTENED_FILES)
 
-$(OUTPUT_DIR)/%_schematic.svg: $(OUTPUT_DIR)/%_flattened_verilog.v
-	python3 $(SCRIPT_DIR)/generate_schematic.py -input_file $< -output $(basename $@)
+$(OUTPUT_DIR)/%_schematic.svg: $(OUTPUT_DIR)/%_flattened_verilog.v $(SCRIPT_DIR)/generate_schematic.py
+	python3 $(SCRIPT_DIR)/generate_schematic.py -input_file $< -output $(basename $@) | tee $<_schematic.log
 	mv parser.out $(OUTPUT_DIR)
 	mv parsetab.py $(OUTPUT_DIR)
 
-$(OUTPUT_DIR)/%_ast.log: $(OUTPUT_DIR)/%_flattened_verilog.v
+$(OUTPUT_DIR)/%_ast.log: $(OUTPUT_DIR)/%_flattened_verilog.v $(SCRIPT_DIR)/ast_understanding.py
 	python3 $(SCRIPT_DIR)/ast_understanding.py -input_file $< | tee $@
 	mv parser.out $(OUTPUT_DIR)
 	mv parsetab.py $(OUTPUT_DIR)
 
-$(OUTPUT_DIR)/%_flattened_verilog.v: $(DESIGN_DIR)/%.v
+$(OUTPUT_DIR)/%_flattened_verilog.v: $(DESIGN_DIR)/%.v $(SCRIPT_DIR)/flatten_verilog.py
 	python3 $(SCRIPT_DIR)/flatten_verilog.py -input_file $< -output $@
 
 clean:
--- script/generate_schematic.py
+++ script/generate_schematic.py
@@ -3,12 +3,37 @@ from pyverilog.vparser.ast import Node, InstanceList, Instance, PortArg
 from graphviz import Digraph
 import argparse
 
-node_name_mapping = {"input":{}, "wire":{}, "output":{}}
+node_name_mapping = {"input":{}, "wire":{"input":{}, "output":{}}, "output":{}}
+
+def extract_always_block_code(filename, start_lineno):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    block_code = []
+    nested_count = 0
+    capturing = False
+    for i, line in enumerate(lines):
+        if i + 1 >= start_lineno:
+            capturing = True
+        if capturing:
+            block_code.append(line)
+            if 'begin' in line:
+                nested_count += 1
+            if 'end' in line:
+                nested_count -= 1
+                if nested_count == 0:
+                    break
+    return ''.join(block_code)
+
+def extract_assign_statement_code(filename, start_lineno):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    return lines[start_lineno - 1]
 
 def create_schematic_from_ast(ast):
     # Initialize a Graphviz Digraph for the schematic
     schematic = Digraph(format='svg')
-    schematic.attr(rankdir='LR')  # Left-to-right layout, orthogonal edges
+    # schematic.attr(rankdir='LR', nodesep="1.0", ranksep="1.0", splines="ortho")  # Left-to-right layout, orthogonal edges
+    schematic.attr(rankdir='LR', nodesep="1.0", ranksep="1.0")  # Left-to-right layout, orthogonal edges
 
     # Cluster for input ports
     with schematic.subgraph(name="cluster_inputs") as inputs_cluster:
@@ -31,7 +56,7 @@ def create_schematic_from_ast(ast):
 
     with schematic.subgraph(name="cluster_outputs") as outputs_cluster:
         outputs_cluster.attr(style="solid", color="blue", label="Outputs", fontsize="12")
-        
+    
         # Traverse the AST to extract input ports
         def add_output_ports(node):
             if (node.__class__.__name__ == 'Output'):
@@ -70,14 +95,17 @@ def create_schematic_from_ast(ast):
                     elif hasattr(port.argname, "name"):
                         external_wire = str(port.argname.name)
                     if str(external_wire) in node_name_mapping["input"]:
-                        schematic.edge(node_name_mapping["input"][str(external_wire)], node_name, label=f"{external_wire}", arrowhead="vee", color="black")
-                    elif external_wire in node_name_mapping["wire"]:
-                        schematic.edge(node_name_mapping["wire"][str(external_wire)], node_name, arrowhead="vee", color="black")
+                        schematic.edge(f'{node_name_mapping["input"][str(external_wire)]}:e', f'{node_name}:w', label=f"{external_wire}", arrowhead="vee", color="black")
+                    elif external_wire in node_name_mapping["wire"]["output"]:
+                        schematic.edge(f'{node_name_mapping["wire"]["output"][str(external_wire)]}:e', f'{node_name}:w', label=f"{external_wire}", arrowhead="vee", color="black")
+                    elif external_wire in node_name_mapping["wire"]["input"]:
+                        schematic.edge(f'{node_name}:e', f'{node_name_mapping["wire"]["input"][str(external_wire)]}:w', label=f"{external_wire}", arrowhead="vee", color="black")
                     elif external_wire in node_name_mapping["output"]:
-                        schematic.edge(node_name, node_name_mapping["output"][str(external_wire)], arrowhead="vee", color="black")
+                        schematic.edge(f'{node_name}:e', f'{node_name_mapping["output"][str(external_wire)]}:w', label=f"{external_wire}", arrowhead="vee", color="black")
                     else:
                         # Update wire mapping for future connections
-                        node_name_mapping["wire"][external_wire] = node_name
+                        # node_name = "wire_" + internal_port
+                        node_name_mapping["wire"]["output"][external_wire] = node_name
 
         # Recursively traverse child nodes
         if hasattr(node, 'children'):
@@ -85,6 +113,138 @@ def create_schematic_from_ast(ast):
                 traverse_ast_for_schematic(child)
 
     traverse_ast_for_schematic(ast)
+
+    def l_value_extractor(node):
+        # print(f'vars in L value = {vars(node)}')
+        # print(f'node in l_value_extractor = {node}')
+        if hasattr(node, 'name'):
+            return str(node.name)
+        if hasattr(node, 'children'):
+            for child in node.children():
+                result = l_value_extractor(child)
+                if result is not None:  # Return as soon as a non-None result is found
+                    return result
+
+    def signal_extractor(node, signal_dict=None):
+        if signal_dict is None:
+            signal_dict = {"input": set(), "output": set()}
+
+        # Check for signal direction based on node attributes
+        if (node.__class__.__name__ == 'Lvalue'):
+            signal_name = l_value_extractor(node)
+            signal_dict["output"].add(signal_name)
+        elif hasattr(node, 'name'):
+            signal_dict["input"].add(node.name)
+
+        # Recursively process child nodes
+        if hasattr(node, 'children'):
+            for child in node.children():
+                signal_extractor(child, signal_dict)
+
+        return signal_dict
+
+    def add_always_blocks_to_schematic(node):
+        if node.__class__.__name__ == 'Always':
+            # Extract the always block code as the node label
+            always_code = extract_always_block_code(args.input_file,node.lineno)
+            always_code = always_code.replace("\n", "\\l")
+            always_node_name = f"always_{id(node)}"
+            
+            # Add the Always block as a node
+            schematic.node(always_node_name, label=always_code, shape="box", style="rounded,filled", color="lightblue", fontsize="10", fontname="Courier")
+            signal_names = signal_extractor(node)
+            signal_names['input'] -= signal_names['output']
+            for input_signal in signal_names["input"]:
+                if input_signal in node_name_mapping["wire"]["input"] and node_name_mapping["wire"]["input"][input_signal] is not None:
+                    node_name_mapping["wire"]["input"][input_signal] = [node_name_mapping["wire"]["input"][input_signal]]
+                    node_name_mapping["wire"]["input"][input_signal].append(always_node_name)
+                else:
+                    node_name_mapping["wire"]["input"][input_signal] = always_node_name
+                if input_signal in node_name_mapping["input"]:
+                    schematic.edge(f'{node_name_mapping["input"][input_signal]}:e', f'{always_node_name}:w', label=f"{input_signal}", arrowhead="vee", color="black")
+                elif input_signal in node_name_mapping["wire"]["output"]:
+                    schematic.edge(f'{node_name_mapping["wire"]["output"][input_signal]}:e', f'{always_node_name}:w', label=f"{input_signal}", arrowhead="vee", color="black")
+                # else:
+                #     # Update wire mapping for future connections
+                #     if input_signal in node_name_mapping["wire"]["input"] and node_name_mapping["wire"]["input"][input_signal] is not None:
+                #         node_name_mapping["wire"]["input"][input_signal] = [node_name_mapping["wire"]["input"][input_signal]]
+                #         node_name_mapping["wire"]["input"][input_signal].append(always_node_name)
+                #     else:
+                #         node_name_mapping["wire"]["input"][input_signal] = always_node_name
+
+            for output_signal in signal_names["output"]:
+                node_name_mapping["wire"]["output"][output_signal] = always_node_name
+                if output_signal in node_name_mapping["wire"]["input"]:
+                    if isinstance(node_name_mapping["wire"]["input"][output_signal], list):
+                        for target_node in node_name_mapping["wire"]["input"][output_signal]:
+                            schematic.edge(f'{always_node_name}:e', f'{target_node}:w', label=f"{output_signal}", arrowhead="vee", color="black")
+                    else:
+                        schematic.edge(f'{always_node_name}:e', f'{node_name_mapping["wire"]["input"][output_signal]}:w', label=f"{output_signal}", arrowhead="vee", color="black")
+                elif output_signal in node_name_mapping["output"]:
+                    schematic.edge(f'{always_node_name}:e', f'{node_name_mapping["output"][output_signal]}:w', label=f"{output_signal}", arrowhead="vee", color="black")
+                # else:
+                #     # Update wire mapping for future connections
+                #     node_name_mapping["wire"]["output"][output_signal] = always_node_name
+
+        # Recursively traverse child nodes
+        if hasattr(node, 'children'):
+            for child in node.children():
+                add_always_blocks_to_schematic(child)
+
+    # Add this call after other AST processing
+    add_always_blocks_to_schematic(ast)
+
+    def add_assign_statements_to_schematic(node):
+        if node.__class__.__name__ == 'Assign':
+            # Extract the always block code as the node label
+            assign_code = extract_assign_statement_code(args.input_file,node.lineno)
+            # always_code = node.to_verilog() if hasattr(node, 'to_verilog') else "Always Block"
+            assign_node_name = f"assign_{id(node)}"
+            
+            # Add the Always block as a node
+            schematic.node(assign_node_name, label=assign_code, shape="box", style="rounded,filled", color="lightblue", fontsize="10", fontname="Courier")
+            signal_names = signal_extractor(node)
+            signal_names['input'] -= signal_names['output']
+            for input_signal in signal_names["input"]:
+                if input_signal in node_name_mapping["wire"]["input"] and node_name_mapping["wire"]["input"][input_signal] is not None:
+                    node_name_mapping["wire"]["input"][input_signal] = [node_name_mapping["wire"]["input"][input_signal]]
+                    node_name_mapping["wire"]["input"][input_signal].append(assign_node_name)
+                else:
+                    node_name_mapping["wire"]["input"][input_signal] = assign_node_name
+                if input_signal in node_name_mapping["input"]:
+                    schematic.edge(f'{node_name_mapping["input"][input_signal]}:e', f'{assign_node_name}:w', label=f"{input_signal}", arrowhead="vee", color="black")
+                elif input_signal in node_name_mapping["wire"]["output"]:
+                    schematic.edge(f'{node_name_mapping["wire"]["output"][input_signal]}:e', f'{assign_node_name}:w', label=f"{input_signal}", arrowhead="vee", color="black")
+                # else:
+                #     # Update wire mapping for future connections
+                #     if input_signal in node_name_mapping["wire"]["input"] and node_name_mapping["wire"]["input"][input_signal] is not None:
+                #         node_name_mapping["wire"]["input"][input_signal] = [node_name_mapping["wire"]["input"][input_signal]]
+                #         node_name_mapping["wire"]["input"][input_signal].append(assign_node_name)
+                #     else:
+                #         node_name_mapping["wire"]["input"][input_signal] = assign_node_name
+
+            for output_signal in signal_names["output"]:
+                node_name_mapping["wire"]["output"][output_signal] = assign_node_name
+                if output_signal in node_name_mapping["wire"]["input"]:
+                    if isinstance(node_name_mapping["wire"]["input"][output_signal], list):
+                        for target_node in node_name_mapping["wire"]["input"][output_signal]:
+                            schematic.edge(f'{assign_node_name}:e', f'{target_node}:w', label=f"{output_signal}", arrowhead="vee", color="black")
+                    else:
+                        schematic.edge(f'{assign_node_name}:e', f'{node_name_mapping["wire"]["input"][output_signal]}:w', label=f"{output_signal}", arrowhead="vee", color="black")
+                elif output_signal in node_name_mapping["output"]:
+                    schematic.edge(f'{assign_node_name}:e', f'{node_name_mapping["output"][output_signal]}:w', label=f"{output_signal}", arrowhead="vee", color="black")
+                # else:
+                #     # Update wire mapping for future connections
+                #     node_name_mapping["wire"]["output"][output_signal] = assign_node_name
+
+
+        # Recursively traverse child nodes
+        if hasattr(node, 'children'):
+            for child in node.children():
+                add_assign_statements_to_schematic(child)
+
+    # Add this call after other AST processing
+    add_assign_statements_to_schematic(ast)
 
     # Save the schematic
     filename_schematic = args.output
@@ -103,3 +263,4 @@ if __name__ == "__main__":
 
     # Generate schematic from AST
     create_schematic_from_ast(ast)
+    # print(f'node_name_mapping = {node_name_mapping}')
